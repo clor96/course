e6c967066ed6e92f65b74049d6f9f2c2608d709c
timur <al.can96@mail.ru>
2019-08-22 16:02:32 +0400

    3 task correction

--- 1.js
+++ 1.js
@@ -11,8 +11,35 @@ function start() {
     money = +prompt('Ваш бюджет на месяц?');
   }
 }
-// start();
 
+function chooseExpenses() {
+  do {
+    a = prompt("Введите обязательную статью расходов в этом месяце");
+    b = +prompt('Во сколько это обойдется?');
+  } while (isFinite(a) || (a == "") || (b == "") || (a.length > 50) || (a == null) && (b == null));
+  appData.expenses[a] = b;
+}
+
+function detectDayBudget() {
+  appData.perDay = Math.round((appData.budget - appData.expenses[a]) / 30);
+  alert("Бюджет на 1 день:" + appData.perDay);
+}
+
+function chooseOptExpenses() {
+  for (var i = 1; i <= 3; i++) {
+    appData.optionalExpenses[i] = prompt('Ваши не обязательные расходы');
+  }
+}
+
+function checkSavings(){
+  if ( appData.savings == true ){
+    var save = +prompt('сколько у вас сбережений, если они у вас есть?'),
+        percent = +prompt('под какой процент?');
+    
+    appData.monthIncome = save/100/12*percent;
+    alert("Столько приходит в месяц: " + appData.monthIncome);
+  }
+}
 
 var appData = {
   budget: money,
@@ -23,38 +50,10 @@ var appData = {
   savings: true,
 };
 
-function chooseExpenses() {
-  do {
-    a = prompt("Введите обязательную статью расходов в этом месяце");
-    b = +prompt('Во сколько это обойдется?');
-  } while (isFinite(a) || (a == "") || (b == "") || (a.length > 50) || (a == null) && (b == null));
-  appData.expenses[a] = b;
-}
-// chooseExpenses();
-
-function detectDayBudget() {
-  appData.perDay = Math.round((appData.budget - appData.expenses[a]) / 30);
-  alert("Бюджет на 1 день:" + appData.perDay);
-}
-// detectDayBudget();
-
-
-function chooseOptExpenses() {
-  for (var i = 1; i <= 3; i++) {
-    appData.optionalExpenses[i] = prompt('Ваши не обязательные расходы');
-  }
-}
-// chooseOptExpenses();
-
-function checkSavings(){
-  if ( appData.savings == true ){
-    var save = +prompt('сколько у вас сбережений, если они у вас есть?'),
-        percent = +prompt('под какой процент?');
-    
-    appData.monthIncome = save/100/12*percent;
-    alert("Столько прихожит в месяц: " + appData.monthIncome);
-  }
-}
+start();
+chooseExpenses();
+detectDayBudget();
+chooseOptExpenses();
 checkSavings();
 
 console.log(appData);
