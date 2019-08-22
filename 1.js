161b2276c8ec8b00595878cfab11dfd5444b6ec8
timur <al.can96@mail.ru>
2019-08-22 15:56:03 +0400

    task 3

--- 1.js
+++ 1.js
@@ -1,26 +1,60 @@
-var money = +prompt('Ваш бюджет на месяц?');
-var time =   prompt('Введите дату в формате YYYY-MM-DD');
+var money,
+  time,
+  a,
+  b;
+
+function start() {
+  money = +prompt('Ваш бюджет на месяц?');
+  time = prompt('Введите дату в формате YYYY-MM-DD');
+
+  while (money == "" || money == null || isNaN(money)) {
+    money = +prompt('Ваш бюджет на месяц?');
+  }
+}
+// start();
+
 
 var appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false,
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: true,
 };
 
-for (var i = 0; i < 1; i++){
-var a =  prompt("Введите обязательную статью расходов в этом месяце"),
+function chooseExpenses() {
+  do {
+    a = prompt("Введите обязательную статью расходов в этом месяце");
     b = +prompt('Во сколько это обойдется?');
-  if ( (typeof(a) === "string") && (a != "") && (b != "") && (a.length < 50 ) && (a !== null) && (b !== null) )
-    appData.expenses[a] = b; 
-        else
-    i--;
+  } while (isFinite(a) || (a == "") || (b == "") || (a.length > 50) || (a == null) && (b == null));
+  appData.expenses[a] = b;
 }
+// chooseExpenses();
+
+function detectDayBudget() {
+  appData.perDay = Math.round((appData.budget - appData.expenses[a]) / 30);
+  alert("Бюджет на 1 день:" + appData.perDay);
+}
+// detectDayBudget();
 
 
-appData.perDay =  Math.round( (appData.budget - appData.expenses[a]) / 30 ) ; 
+function chooseOptExpenses() {
+  for (var i = 1; i <= 3; i++) {
+    appData.optionalExpenses[i] = prompt('Ваши не обязательные расходы');
+  }
+}
+// chooseOptExpenses();
 
-alert("Бюджет на 1 день:" + appData.perDay );
+function checkSavings(){
+  if ( appData.savings == true ){
+    var save = +prompt('сколько у вас сбережений, если они у вас есть?'),
+        percent = +prompt('под какой процент?');
+    
+    appData.monthIncome = save/100/12*percent;
+    alert("Столько прихожит в месяц: " + appData.monthIncome);
+  }
+}
+checkSavings();
 
+console.log(appData);
